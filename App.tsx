a36fd46ecc9c6ab9cf28b4125bb74c1beb268d2e
ry0-cha-ng <ry0changmw@gmail.com>
2025-04-22 04:44:55 +0900

    :sparkles: first commit

--- App.tsx
+++ App.tsx
@@ -1,42 +1,33 @@
 import './shim';
 import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-// import sampleCode from './services/symbol';
-// import { Configuration, NodeRoutesApi } from './services/NodeClientService';
-// import { getRandomBytes } from './src/utils/crypto';
 import { PrivateKey } from 'symbol-sdk';
-import { Network, SymbolFacade } from 'symbol-sdk/symbol';
+import { Network, SymbolAccount, SymbolFacade } from 'symbol-sdk/symbol';
 
 export default function App() {
+  const [account, setAccount] = useState<SymbolAccount | null>(null);
+
   const generateAccount = () => {
     const facade = new SymbolFacade(Network.MAINNET);
     // ランダムな秘密鍵を生成
     const privateKey = PrivateKey.random();
-    console.log('privateKey created', privateKey);
     
     // アカウントを生成
     const account = facade.createAccount(privateKey);
-    console.log('generateAccount end', account);
+    return account;
   }
+
   useEffect(() => {
-    // Run sample code
-    // sampleCode();
-    console.log('generateAccount start');
-    generateAccount();
-    console.log('generateAccount end');
-    // Create Node Client Sample
-    // const config = new Configuration({ basePath: 'https://symbolnode.blockchain-authn.app:3001' });
-    // const nodeClient = new NodeRoutesApi(config);
-    // nodeClient
-    //   .getNodeHealth()
-    //   .then((e) => console.log(e))
-    //   .catch(() => console.log('node unhealthy'));
+    const account = generateAccount();
+    setAccount(account);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>symbol-sdk-expo-sample</Text>
+      <Text>generateAccount</Text>
+      <Text>address: {account?.address.toString()}</Text>
+      <Text>publicKey: {account?.publicKey.toString()}</Text>
       <StatusBar style='auto' />
     </View>
   );
